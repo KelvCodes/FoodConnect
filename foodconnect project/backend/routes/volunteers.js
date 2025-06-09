7fee7c246dff3ce350c0cdefdb5565fb417bf6b4
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-06-09 11:59:49 -0700

    Update volunteers.js

--- foodconnect project/backend/routes/volunteers.js
+++ foodconnect project/backend/routes/volunteers.js
@@ -1,4 +1,4 @@
-const express = require('express');
+
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const Volunteer = require('../models/Volunteer');
